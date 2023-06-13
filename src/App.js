e13d0a5ffa58c3702ab1ce6f4471adbeeb37c1db
Ngaremaina <ngaremaina4@gmail.com>
2023-06-13 09:28:16 +0300

    Completed Application

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 
 
   const fetchData = async () => {
-    const response = await fetch("http://127.0.0.1:8000/")
+    const response = await fetch("https://youanime.onrender.com/")
     const jsonData = await response.json()
     setAnimations(jsonData)
   }
