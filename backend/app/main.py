cfff53c734d45d170909defa8242456d4424a0ee
Ngare Maina <ngaremaina4@gmail.com>
2024-07-30 13:30:16 +0300

    Youanime App

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,170 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from model import Animations, Genres, session, Directors, Customers
+from typing import Optional
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello from FastAPI"}
+from fastapi.middleware.cors import CORSMiddleware
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "https://app.netlify.com/",
+    "https://youanime.netlify.app",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class AnimationSchema(BaseModel):
+    title:str
+    plot:str
+    year:str
+    cover:str
+    type:str
+    video_link:str
+
+    directors_id:int
+    genres_id:int
+
+    class Config:
+        orm_mode = True
+
+class UpdateAnimationSchema(BaseModel):
+    title:Optional[str] 
+    plot:Optional[str] 
+    year:Optional[str] 
+    cover:Optional[str] 
+    type:Optional[str] 
+    video_link:Optional[str] 
+
+    directors_id:Optional[int] 
+    genres_id:Optional[int] 
+
+
+    class Config:
+        orm_mode = True
+class GenreSchema(BaseModel):
+    name:str
+    class Config:
+        orm_mode = True
+
+class DirectorsSchema(BaseModel):
+    first_name:str
+    last_name:str
+    email :str
+    phone_number:str
+    gender:str
+    age:int
+    class Config:
+        orm_mode = True
+
+class UpdateAnimationSchema(BaseModel):
+    title:Optional[str]
+    plot:Optional[str]
+    year:str
+    cover:str
+    type:str
+    video_link:str
+
+    directors_id:int
+    genres_id:int   
+
+class CustomerSchema(BaseModel):
+    first_name:str
+    last_name:str
+    email :str
+    phone_number:str
+    gender:str
+    age:int
+    password:str
+    class Config:
+        orm_mode = True
+
+
+@app.get('/')
+def getAnimation():
+    return session.query(Animations).all()
+
+@app.get('/{id}')
+def getAnimation(id:int):
+    return session.query(Animations).filter_by(id=id).first()
+
+@app.post('/addanimations')
+def addAnimation(animation:AnimationSchema):
+    anime= Animations(**dict(animation))
+    session.add(anime)
+    session.commit()
+    return {"detail":"Added successfully"}
+
+@app.delete('/deleteanimation/{id}')
+def deleteAnimation(id:int) -> None:
+    anime = session.query(Animations).filter_by(id=id).first()
+    session.delete(anime)
+    session.commit()
+    return {"detail":f"Animation with id {id} deleted successfully"}
+
+@app.put('/putanimation/{id}')
+def animationPut(id:int, payload:UpdateAnimationSchema):
+    anime = session.query(Animations).filter_by(id=id).first()
+    for key, value in dict(payload).items():
+        setattr(anime, key, value)
+    session.commit()
+    return {"detail":f"Animation with id {id} updated successfully"}
+
+@app.patch('/patchanimations/{id}')
+def animationPatch(id:int, payload:UpdateAnimationSchema):
+    anime = session.query(Animations).filter_by(id=id).first()
+    for key, value in dict(payload).items():
+        setattr(anime, key, value)
+    session.commit()
+    return {"detail":f"Animation with id {id} patched successfully"}
+
+@app.get('/getgenres/${genid}')
+def getGenres(genid:int):
+    return session.query(Genres).filter_by(id=genid).first()
+
+@app.get('/getgenres/')
+def getGenres():
+    return session.query(Genres).all()
+
+@app.post('/addgenres')
+def addGenres(genres:GenreSchema):
+    genre = Genres(**dict(genres))
+    session.add(genre)
+    session.commit()
+    return genre
+
+@app.post('/adddirectors')
+def addDirectors(directors:DirectorsSchema):
+    director = Directors(**dict(directors))
+    session.add(director)
+    session.commit()
+
+@app.get('/getdirectors/')
+def getDirectors():
+    return session.query(Directors).all()
+
+@app.get('/getdirectors/${id}')
+def getDirector(id:int):
+    return session.query(Directors).filter_by(id=id).first()
+
+@app.post('/addcustomers')
+def addCustomers(customers:CustomerSchema):
+    customer = Customers(**dict(customers))
+    session.add(customer)
+    session.commit()
+
+@app.get('/getcustomers')
+def getAllCustomers():
+    return session.query(Customers).all()
+
+@app.get('/getcustomers/{id}')
+def getCustomer(id:int):
+    return session.query(Customers).filter_by(id=id).first()
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,108 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import AnimationList from './components/AnimationList';
+import { Routes, Route } from 'react-router-dom';
+import NavHeader from './components/NavHeader';
+import DetailsPage from './components/DetailsPage';
+import AddAnimation from './components/AddAnimation';
+import EditAnimation from './components/EditAnimation';
+import Login from './components/DirectorSignUp';
+import SignUp from './components/CustomerSignUp';
+import Series from './components/Series';
+import Movies from './components/Movies';
+
 
 function App() {
+  const [animations, setAnimations] = useState([])
+  //Fetch all animations from the database
+  useEffect(() => {   
+    fetchData()    
+  }, []);
+
+  //Function that fetches all animations
+  const fetchData = async () => {
+    const response = await fetch("https://youanime.onrender.com/")
+    const jsonData = await response.json()
+    setAnimations(jsonData)
+  }
+
+  //Function that adds a new animation to the database
+  const addAnimation = (data) => {
+    fetch("https://youanime.onrender.com/addanimations", {
+      method:"POST",
+      headers: {"Content-Type":"application/json", "Accept":"application/json"},
+      body:JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))   
+  }
+  //Function that patches the animation
+  const editAnimation = (data, id) => {
+    fetch(`https://youanime.onrender.com/patchanimations/${id}`, {
+      method:"PATCH",
+      headers: {"Content-Type":"application/json", "Accept":"application/json"},
+      body:JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))   
+
+  }
+  //Function that deletes the animation
+  const deleteAnimation = (id) => {
+    fetch(`https://youanime.onrender.com/deleteanimation/${id}`,{
+      method:"DELETE"
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+  //Function that adds customers to the database
+  const addCustomer = (data) => {
+    fetch("https://youanime.onrender.com/addcustomers", {
+      method:"POST",
+      headers: {"Content-Type":"application/json", "Accept":"application/json"},
+      body:JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))   
+
+  }
+   //Function that adds customers to the database
+  const addDirector = (data) => {
+    fetch("https://youanime.onrender.com/adddirectors", {
+      method:"POST",
+      headers: {"Content-Type":"application/json", "Accept":"application/json"},
+      body:JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))   
+
+  }
+  //Function that searches the animation by its title
+  const searchAnimation = (search) => {
+    const fetchResults = animations.filter(animation => animation.title.toLowerCase().includes(search.toLowerCase()))
+    return setAnimations(fetchResults)
+  }
+  //Function that sorts the animations by the title
+  function sortByTitle() {
+    const sortedAnimations = [...animations].sort((a, b) => a.title.localeCompare(b.title));
+    setAnimations(sortedAnimations);
+  }
+  
   return (
+    //Display the routes to the components
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavHeader sortByTitle={sortByTitle}/>
+      <Routes>
+        <Route path='/' element={ <AnimationList animations = {animations} searchAnimation={searchAnimation}/>}></Route>
+        <Route path='/:id' element = {<DetailsPage deleteAnimation = {deleteAnimation} />}></Route>
+        <Route path = '/movies' element={<Movies  animations = {animations}/>}></Route>
+        <Route path='/series' element={<Series  animations = {animations} />}></Route>
+        <Route path='/addanimation' element = { <AddAnimation addAnimation={addAnimation} /> }></Route>
+        <Route path='/editanimation/:id' element = { <EditAnimation editAnimation = {editAnimation} />}></Route>
+        <Route path='/directorsignup' element={<Login addDirector={addDirector}/>}></Route>
+        <Route path='/customersignup' element={<SignUp addCustomer={addCustomer} />}></Route>
+      </Routes>
+     
     </div>
   );
 }
